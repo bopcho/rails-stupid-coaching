e1bcb2e37c02ca155708d7931e68be18e40a4b86
Boyan Asenov <ivanoffboyan@gmail.com>
2022-05-16 16:23:39 +0200

    stupid

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,14 +4,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-     if params[:question] == "i am going to work right now!"
-
+    if params[:question] == "i am going to work right now!"
+      @answer = "Great!"
     elsif params[:question].end_with?("?")
-     @answer = "Silly question, get dressed and go to work!"
+      @answer = "Silly question, get dressed and go to work!"
     else
-       @answer = "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
-
   end
 end
 
